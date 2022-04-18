a16ca7386c39c3006571718bc4cf4c5e9dd4c736
Titus Njiru <njirutitus@gmail.com>
2022-04-18 15:43:28 +0300

    update to include extracted header and footer

--- index.php
+++ index.php
@@ -1,47 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
-    <title>Document</title>
-</head>
-<body>
-    <nav>
-        <a href="index.php">Home</a>
-        <a href="view.php">View Messages</a>
-    </nav>
-    <div class="container">
-        <h1>Contact System</h1>
-        <?php
-        session_start();
-         if(isset($_SESSION['success'])){
-             echo '<p class="success">'.$_SESSION['success'].'</p>';
-             unset($_SESSION['success']);
-         }
-            if(isset($_SESSION['error'])){
-                echo '<p class="error">'.$_SESSION['error'].'</p>';
-                unset($_SESSION['error']);
-            }
+<?php include_once 'header.php'; ?>
 
-        ?>
-        <form action="save.php" method="post">
-            <div class="form-group">
+<div class="container">
+    <h1>Contact System</h1>
+    <?php include_once 'feedback.php';?>
+    <form action="save.php" method="post">
+        <div class="grid">
+            <div>
                 <label for="name">Name</label>
-                <input type="text" name="name" id="name" class="form-control">
+                <input type="text" name="name" id="name">
             </div>
-            <div class="form-group">
+
+            <div>
                 <label for="email">Email</label>
-            <input type="email" name="email" id="email" class="form-control">
+                <input type="email" name="email" id="email">
+            </div>
         </div>
-        <div class="form-group">
+        <div>
             <label for="message">Message</label>
-            <textarea name="message" id="message" class="form-control"></textarea>
+            <textarea name="message" id="message"></textarea>
+            <span>
         </div>
-        <div class="form-group">
-            <button type="submit" class="btn btn-primary">Submit</button>
+        <div>
+            <button type="submit">Submit</button>
         </div>
-    </div>
-</body>
-</html>
+</div>
+<?php include_once 'footer.php'; ?>
