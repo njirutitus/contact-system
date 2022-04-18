74edb705390cdad94066e50e405bc7d99f457cc1
Titus Njiru <njirutitus@gmail.com>
2022-04-18 16:11:50 +0300

    ignore stared sessions

--- connect.php
+++ connect.php
@@ -7,6 +7,10 @@
   try {
       $db = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        if(!isset($_SESSION)) 
+        { 
+            session_start(); 
+        }
   } catch (PDOException $e) {
       echo 'Connection failed: ' . $e->getMessage();
   }
