fd4944222efe1d484812888370b514fd817cc63d
Titus Njiru <njirutitus@gmail.com>
2022-04-18 15:40:40 +0300

    extract footer layout section

---
+++ footer.php
@@ -0,0 +1,35 @@
+<footer>
+    <div class="container">
+        <div class="grid">
+            <div class="col-md-4">
+                <span class="copyright">Copyright &copy; <a href="https://titusnjiru.dev/" target="_blank">Titus
+                        Njiru</a>
+                    2022</span>
+            </div>
+            <div>
+                <a href="https://github.com/njirutitus">
+                    <i class="fa fa-github"></i>
+                </a> &nbsp;
+                <a href="#">
+                    <i class="fa fa-instagram"></i>
+                </a>&nbsp;
+                <a href="https://www.linkedin.com/in/titusnjiru">
+                    <i class="fa fa-linkedin"></i>
+                </a>&nbsp;
+            </div>
+            <div>
+                <ul style="list-style: none !important;">
+                    <li>
+                        <a href="#">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="#">Terms of Use</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+
+</html>
