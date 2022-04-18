f7a22d2e434165ad307feef9559804b95a977a96
Titus Njiru <njirutitus@gmail.com>
2022-04-18 15:42:41 +0300

    add validation to save script

--- save.php
+++ save.php
@@ -6,12 +6,35 @@
      $name = $_POST['name'];
      $email = $_POST['email'];
      $message = $_POST['message'];
-    try {
-     $stmt = $db->prepare("INSERT INTO feedback (name, email, message) VALUES (?,?,?)");
-    $stmt->execute([$name, $email, $message]);
-    $_SESSION['success'] = 'Message sent successfully';
-    } catch (PDOException $e) {
-        $_SESSION['error'] = $e->getMessage();
+
+    //  Validate data
+    $errors = [];
+    if(empty($name)){
+        $errors['name'] = 'Name is required';
+    }
+    if(empty($email)){
+        $errors['email'] = 'Email is required';
+    }
+    if(empty($message)){
+        $errors['message'] = 'Message is required';
+    }
+    if(!empty($errors)){
+        $_SESSION['error'] = "All fields are required";
+        $_SESSION['errors'] = $errors;
+        $_SESSION['old_data'] = $_POST;
+        header('Location: index.php');
+        exit;
+    }
+
+    //  Insert data into database
+    else {
+        try {
+        $stmt = $db->prepare("INSERT INTO feedback (name, email, message) VALUES (?,?,?)");
+        $stmt->execute([$name, $email, $message]);
+        $_SESSION['success'] = 'Message sent successfully';
+        } catch (PDOException $e) {
+            $_SESSION['error'] = $e->getMessage();
+        }
     }
  }
 
