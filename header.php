cf3858651933e5c036b8d1dee96850db06a9fd1d
Titus Njiru <njirutitus@gmail.com>
2022-04-18 15:40:12 +0300

    extract header layout section

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+    <script src="https://kit.fontawesome.com/9ad9a54963.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="view.php">View Messages</a>
+    </nav>
